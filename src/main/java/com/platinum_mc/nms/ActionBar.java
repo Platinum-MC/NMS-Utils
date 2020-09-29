a90ff6dcb430bb8c54024cc16e75c7bb7bc2d749
A248 <anandebeh@gmail.com>
2020-09-29 16:09:37 -0400

    Add license header

--- src/main/java/com/platinum_mc/nms/ActionBar.java
+++ src/main/java/com/platinum_mc/nms/ActionBar.java
@@ -1,3 +1,21 @@
+/* 
+ * PMC-NMS-Utils
+ * Copyright © 2020 Platinum MC
+ * 
+ * PMC-NMS-Utils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PMC-NMS-Utils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PMC-NMS-Utils. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
+ */
 package com.platinum_mc.nms;
 
 import java.util.Objects;
